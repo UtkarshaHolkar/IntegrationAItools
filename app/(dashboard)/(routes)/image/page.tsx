bd536572fc2721ddf5d1b285373c17accf682abc
UtkashaHolakr <utkarshaaholkar@gmail.com>
2023-08-21 23:24:17 +0530

    second  committ

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -215,7 +215,3 @@ const onSubmit = async (values: z.infer<typeof formSchema>) => {
 
 export default ImagePage;
 
-//shashankkkkkkkkk
-//shashankkkkkkkkk
-//shashankkkkkkkkk
-//shashankkkkkkkkk
--- next.config.js
+++ next.config.js
@@ -9,7 +9,3 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-// shashank is my boy
-// shashank is my boy
-// shashank is my boy
-// shashank is my boy
